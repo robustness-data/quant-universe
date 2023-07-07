fd9ba3d5511d94be4a3246d0919d54889331f658
mercer <120003012+robustness-data@users.noreply.github.com>
2023-07-07 15:21:23 -0700

    enhanced ETF holdings app

--- src/app/pages/1_ETF_Holdings.py
+++ src/app/pages/1_ETF_Holdings.py
@@ -8,30 +8,52 @@ import os
 print(__file__)
 ROOT_DIR=Path(__file__).parent.parent.parent.parent
 
-# Load data
-df = pd.read_csv(ROOT_DIR/"data/equity_market/1_etf/ishares_holdings_2023-06-27.csv")
 
-# Convert the 'Weight (%)' column to numeric, handling any errors that might arise
-df['Weight (%)'] = pd.to_numeric(df['Weight (%)'], errors='coerce')
+@st.cache_data
+def get_etf_holdings_data(dt: str):
+    df = pd.read_csv(ROOT_DIR/f"data/equity_market/1_etf/ishares_holdings_{dt}.csv")
+    # Convert the 'Weight (%)' column to numeric, handling any errors that might arise
+    df['Weight (%)'] = pd.to_numeric(df['Weight (%)'], errors='coerce')
+    return df
 
-# Group by 'Sector' and 'etf_ticker' and calculate the total weight
-grouped_df = df.groupby(['Sector', 'etf_name'])['Weight (%)'].sum().reset_index()
 
-# Pivot the data for the heatmap
-pivot_df = grouped_df.pivot(columns='Sector', index='etf_name', values='Weight (%)')
+@st.cache_data
+def get_etf_sector_weights(etf_holdings_df):
+    # Group by 'Sector' and 'etf_ticker' and calculate the total weight
+    grouped_df = etf_holdings_df.groupby(['Sector', 'etf_name'])['Weight (%)'].sum().reset_index()
 
-# Create heatmap
-fig = px.imshow(pivot_df,
-                labels=dict(x="Sector", y="ETF Name", color="Weight (%)"),
-                x=pivot_df.columns, y=pivot_df.index,
-                text_auto=True, aspect='auto')
+    # Pivot the data for the heatmap
+    pivot_df = grouped_df.pivot(columns='Sector', index='etf_name', values='Weight (%)')
+    return pivot_df
 
-fig.update_layout(
-    title_text="<b>Sector Weights of iShares ETFs</b>",  # Title in bold
-    title_x=0,  # Align title to center
-    autosize=True,  # The graph will resize itself to the size of the container
-    height=500, width=800  # You can adjust the height to your preference
-)
+
+def etf_sector_heatmap(pivot_df):
+    # Create heatmap
+    fig = px.imshow(pivot_df, zmin=0, zmax=100,
+                    labels=dict(x="Sector", y="ETF Name", color="Weight (%)"),
+                    x=pivot_df.columns, y=pivot_df.index,
+                    text_auto=True, aspect='auto')
+
+    fig.update_layout(
+        title_text="<b>Sector Weights of iShares ETFs</b>",  # Title in bold
+        title_x=0,  # Align title to center
+        autosize=True,  # The graph will resize itself to the size of the container
+        height=50*len(pivot_df.index), width=800  # You can adjust the height to your preference
+    )
+
+    return fig
+
+
+def get_all_available_dates():
+    files=os.listdir(ROOT_DIR/f"data/equity_market/1_etf")
+    available_dates=list(set([f.replace('ishares_holdings_','').replace('.csv','') for f in files if '.csv' in f]))
+    available_dates.sort()
+    return available_dates
+
+
+dt=st.selectbox("Date of analysis",get_all_available_dates())
+etf_holdings_df=get_etf_holdings_data(dt)
+etf_sector_weights=get_etf_sector_weights(etf_holdings_df)
 
 # Show the plot using streamlit
-st.plotly_chart(fig)
+st.plotly_chart(etf_sector_heatmap(etf_sector_weights))
