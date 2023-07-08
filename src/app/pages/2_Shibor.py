d3a7b8ec7183c084f7d9b233de28ebc9e6ec9c04
mercer <120003012+robustness-data@users.noreply.github.com>
2023-07-08 00:15:34 -0700

    update shibor graph

--- src/app/pages/2_Shibor.py
+++ src/app/pages/2_Shibor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 
 from pathlib import Path
 import os
@@ -18,17 +19,22 @@ def prepare_shibor_quotes():
     df.columns.name='Tenor'
 
     quotes_long=df.set_index(['Contributor','Date']).stack().rename('ShiborQuote').reset_index()
-    quotes_diff_wide=(
+    quotes_diff_wide_d=(
         quotes_long.set_index(['Date','Contributor','Tenor']).ShiborQuote
         .unstack().unstack().sort_index(ascending=True).diff()
     )
-    quotes_diff_long=quotes_diff_wide.stack().stack().rename('ShiborQuote').reset_index()
-    corr_banks = quotes_diff_wide.ewm(halflife=30).corr()
+    quotes_diff_wide_w=(
+        quotes_long.set_index(['Date', 'Contributor', 'Tenor']).ShiborQuote
+        .unstack().unstack().sort_index(ascending=True).resample('W-FRI').last().diff()
+    )
+    quotes_diff_long=quotes_diff_wide_d.stack().stack().rename('ShiborQuote').reset_index()
 
-    return quotes_long, quotes_diff_long, quotes_diff_wide, corr_banks
+    return quotes_long, quotes_diff_long, quotes_diff_wide_d, quotes_diff_wide_w
 
 
-def get_corr_banks(corr_banks, dt:str, tenor: str):
+@st.cache_resource
+def get_corr_banks(quotes_diff_wide, dt:str, tenor: str, ewm_hl:int):
+    corr_banks = quotes_diff_wide.ewm(halflife=ewm_hl).corr()
     corr_df=corr_banks.xs(dt).loc[tenor, tenor]
     corr_df.index.name='Bank-x'
     corr_df.columns.name='Bank-y'
@@ -54,12 +60,69 @@ def plotly_heatmap(pivot_df, val_col, x_label=None, y_label=None, title=None):
     return fig
 
 
-quotes_long, quotes_diff_long, quotes_diff_wide, corr_banks = prepare_shibor_quotes()
+quotes_long, quotes_diff_long, quotes_diff_wide_d, quotes_diff_wide_w = prepare_shibor_quotes()
+
+# correlation plot of quotes change
 tenor = st.selectbox('Tenor', ("O/N", "1W", "2W", "1M", "3M", "6M", "9M", "1Y") )
-dt=st.selectbox('Date', tuple([x.date().isoformat() for x in quotes_long.Date.unique().tolist()]) )
+freq=st.selectbox("Return frequency:",('Weekly','Daily'))
+shibor_ret_data_map={'Daily': quotes_diff_wide_d, 'Weekly': quotes_diff_wide_w}
+shibor_rel_dates=tuple([x.date().isoformat()
+                        for x in shibor_ret_data_map.get(freq).sort_index(ascending=False).index[1:].unique().tolist()])
+dt=st.selectbox('Date', index=0, options=shibor_rel_dates)
+ewm_hl=st.slider("Half-life of EWM Correlation in Days:", min_value=30, max_value=180, step=5, value=30)
+
 st.write(
     plotly_heatmap(
-        get_corr_banks(corr_banks,dt,tenor),
+        get_corr_banks(shibor_ret_data_map.get(freq),dt,tenor,ewm_hl),
         val_col='Correlation (%)',
         title=f"Correlation of Shibor Quote Changes for Tenor {tenor} on {dt}")
 )
+
+
+# Plot time series of SHIBOR quotes
+def plot_shibor_quotes_ts(quotes_range, tenor):
+
+    quotes_range_tau = quotes_range.set_index('Tenor').xs(tenor)
+
+    # Create a plotly figure
+    fig = go.Figure()
+
+    # Add a trace for the median line
+    fig.add_trace(go.Scatter(
+        x=quotes_range_tau['Date'],
+        y=quotes_range_tau['median'],
+        mode='lines',
+        name='Median',
+        line=dict(color='black')))
+
+    # Add a trace for the shaded area representing the range
+    fig.add_trace(
+        go.Scatter(
+            x=pd.concat([quotes_range_tau['Date'],quotes_range_tau['Date'][::-1]]),
+            y=pd.concat([quotes_range_tau['max'],quotes_range_tau['min'][::-1]]),
+            fill='toself',
+            fillcolor='rgba(128,128,128,0.3)',
+            line=dict(color='rgba(255,255,255,0)'),
+            hoverinfo="skip",
+            name='Range'
+        )
+    )
+
+    # Set plot title and labels
+    fig.update_layout(
+        title_text=f'{tenor} Tenor Shibor Quotes Over Time',
+        xaxis_title='Date',
+        yaxis_title='Quote (%)',
+        autosize=False,
+        width=800,
+        height=500,
+        showlegend=True)
+
+    return fig
+
+# Group the data by date and calculate the minimum, maximum, and median
+quotes_range = quotes_long.groupby(['Tenor','Date']).ShiborQuote.agg(['min', 'max', 'median']).reset_index()
+#st.write(quotes_range)
+
+tenor_shibor_ts=st.selectbox("Tenor of Shibor Quotes:",  ("O/N", "1W", "2W", "1M", "3M", "6M", "9M", "1Y") )
+st.plotly_chart(plot_shibor_quotes_ts(quotes_range, tenor_shibor_ts))
