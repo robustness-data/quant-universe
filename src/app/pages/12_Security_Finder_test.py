13af18d7d9b6b88010a9080a45b17a068aa77146
mercer <120003012+robustness-data@users.noreply.github.com>
2023-08-02 03:11:48 -0700

    update app

--- src/app/pages/12_Security_Finder_test.py
+++ src/app/pages/12_Security_Finder_test.py
@@ -1,6 +1,6 @@
 import os, sys, logging
 from pathlib import Path
-ROOT_DIR = Path(__file__).parent.parent.parent
+ROOT_DIR = Path(__file__).parent.parent.parent.parent
 if str(ROOT_DIR) not in sys.path:
     sys.path.append(str(ROOT_DIR))
     #sys.path.append(str(ROOT_DIR/'src'))
@@ -9,7 +9,6 @@ import src.config as cfg
 import src.data.utils as hp
 from src.data.equity_api_yfinance import Stock
 
-
 import datetime
 import itertools
 from tqdm import tqdm
@@ -17,26 +16,26 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+us_universe_df = pd.read_csv(ROOT_DIR/'data'/'equity_market'/'3_fundamental'/'raw'/'us_2023-07-10.csv')
+all_us_tics = us_universe_df.Ticker.unique().tolist()
+
+tic=st.selectbox("Select a Ticker", [None]+all_us_tics)
+
+if tic is None:
+    st.stop()
+
 #asset = Stock.load_stock('300750.SZ')
-asset = Stock.load_stock('TEL')
+asset = Stock.load_stock(tic)
 asset.history(period='max')
 asset.get_financials()
 
+tab_names = ['Description', 'Performance', 'Financials', 'Module Inspector']
+tab_desc, tab_perf, tab_fin, tab_test = st.tabs(tab_names)
 
-
-#st.write(get_method_names(asset))
-#tab_description = st.expander('Description')
-#with tab_description:
-#    st.write(asset.info)
-
-tab_names = ['Description', 'Performance', 'Financials', 'Earnings', 'Dividends', 'Module Inspector']
-tab_desc, tab_perf, tab_fin, tab_earn, tab_div, tab_test = st.tabs(tab_names)
 with tab_desc:
     st.write(asset.info)
     yf_info_type = pd.DataFrame({'info_type': {k: type(v).__name__ for k, v in asset.info.items()}}).info_type.to_dict()
     info_df = pd.DataFrame({'meta_info':{k: asset.info.get(k) for k, v in yf_info_type.items() if v in ('str', 'float', 'int')}})
-    st.dataframe(info_df)
-
 
     def display_company_info(company_info):
         st.title(company_info.get("longName", 'N/A'))
@@ -53,35 +52,8 @@ with tab_desc:
 
 with tab_perf:
     st.title("Performance")
-    #st.write(asset._history)
     hist_price = asset._history.rename(pd.to_datetime).resample('B').ffill().reset_index()
-    st.write(hist_price.columns.tolist())
-
-    import plotly.graph_objects as go
-    from plotly.subplots import make_subplots
-    # Create subplot with 2 rows
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
-                vertical_spacing=0.1, subplot_titles=('Stock Price', 'Volume'), 
-                row_heights=[0.8, 0.2])
-
-    df = hist_price
-    # Add candlestick track on 1st plot
-    fig.add_trace(
-        go.Candlestick(x=df['Date'], open=df['Open'], high=df['High'], 
-                       low=df['Low'], close=df['Close'], name='Price'),
-        row=1, col=1
-    )
-
-    # Add volume bar chart on 2nd plot
-    fig.add_trace(go.Bar(x=df['Date'], y=df['Volume'], showlegend=False), row=2, col=1)
-
-    # Add range slider
-    fig.update_layout(xaxis=dict(
-        rangeslider=dict(visible=True),
-        type='date'
-    ))
-
-    st.plotly_chart(fig)
+    st.plotly_chart(hp.plot_line_chart(hist_price))
 
 with tab_test:
     st.title("Module Inspector")
