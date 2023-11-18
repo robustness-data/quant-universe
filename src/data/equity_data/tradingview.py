fec0a9daf03848e55e13b4b2d2300761aaa329af
mercer <120003012+robustness-data@users.noreply.github.com>
2023-11-18 11:14:22 -0800

    performance attribution and plotting for tradingview.py

--- src/data/equity_data/tradingview.py
+++ src/data/equity_data/tradingview.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 import src.config as cfg
 from src.data.database.db_manager import TradingViewDB
 from src.utils.pandas_utils import df_filter, set_cols_numeric
+from src.utils.general_utils import assign_market_cap_group, check_group_by_input
+import plotly.express as px
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -147,6 +151,109 @@ class TradingView:
             'data_dt': data_dt,
             'available_univ_labels': available_univ_labels
         }
+
+    @staticmethod
+    def define_asset_tags(data):
+        data['IsETF'] = data['Description'].str.contains('ETF')
+        data['MarketCapGroup'] = data['Market Capitalization'].apply(lambda x: assign_market_cap_group(x / 1e6))
+        return data
+
+    @staticmethod
+    @check_group_by_input
+    def calc_average_by_group(data: pd.DataFrame, groupby: list, vars: list, weight: str, reset_index: bool = True):
+        """
+        Calculate the grouped statistics of the columns across the groupby columns
+        parameters:
+            data: pd.DataFrame. A long-format dataframe with the columns to be grouped and calculated
+            groupby: list: the columns to be grouped
+            vars: list: the columns to be calculated
+            reset_index: bool
+        """
+        if weight is None:
+            grouped_mean = data.groupby(groupby)[vars].mean()
+        else:
+            dflist = []
+            for var in vars:
+                grouped_mean_var = data.groupby(groupby).apply(
+                    lambda df: np.average(df[var], weights=df[weight], axis=0)).rename(var)
+                dflist.append(grouped_mean_var)
+            grouped_mean = pd.concat(dflist, axis=1)
+        if reset_index:
+            grouped_mean = grouped_mean.reset_index()
+        return grouped_mean
+
+    @staticmethod
+    def plot_performance_heatmap(data, vars_of_interest: list, groupby: list, drilldown: list,
+                                 xaxis: str, yaxis: str,
+                                 xaxis_labels=None, yaxis_labels=None,
+                                 weight='Market Capitalization', wins: list = None, rounding=None,
+                                 dropna=True, fillna=None, dtype=None, height=600, width=1200):
+
+        perf_by_group = TradingView.calc_average_by_group(data=data, groupby=groupby,
+                                                  vars=vars_of_interest, weight=weight, reset_index=False)
+
+        for c in vars_of_interest:
+            perf_grid = perf_by_group.copy(deep=True)
+            for d in drilldown:
+                perf_grid = perf_grid.xs(d)
+            if fillna:
+                perf_grid = perf_grid.fillna(fillna)
+            if dtype:
+                perf_grid = perf_grid.astype(dtype)
+            if wins:
+                try:
+                    perf_grid = perf_grid.clip(wins[0], wins[1])
+                except:
+                    logger.error(f"Failed to clip {c} to {wins}")
+            if rounding:
+                perf_grid = perf_grid.apply(lambda x: round(x, rounding))
+
+            # formatting performance grid
+            perf_grid = perf_grid.unstack(xaxis)[c]
+            if xaxis_labels:
+                perf_grid = perf_grid.reindex(xaxis_labels, axis=1)
+            if yaxis_labels:
+                perf_grid = perf_grid.reindex(yaxis_labels, axis=0)
+            if dropna:
+                perf_grid = perf_grid.dropna(axis=0, how='all')
+
+            fig = px.imshow(perf_grid,
+                            labels=dict(y=yaxis, x=xaxis, color=c),
+                            x=perf_grid.columns, y=perf_grid.index,
+                            color_continuous_scale='RdBu_r',
+                            color_continuous_midpoint=0,
+                            width=width, height=height,
+                            text_auto=True)
+            fig.update_layout(title_text=c)
+            fig.show()
+
+    @staticmethod
+    def plot_asset_count_heatmap(data: pd.DataFrame, groupby: list, drilldown: list,
+                                 xaxis: str, yaxis:str,
+                                 xaxis_labels=None, yaxis_labels=None,
+                                 fillna=None, dtype=None):
+        asset_count_by_group = data.groupby(group_keys=groupby).count()['Ticker']
+        data_to_plot = asset_count_by_group.copy()
+        for d in drilldown:
+            data_to_plot = data_to_plot.xs(d)
+        data_to_plot = data_to_plot.unstack(xaxis)
+        if xaxis_labels:
+            data_to_plot = data_to_plot.reindex(xaxis_labels, axis=1)
+        if yaxis_labels:
+            data_to_plot = data_to_plot.reindex(yaxis_labels, axis=0)
+        if fillna:
+            data_to_plot = data_to_plot.fillna(fillna)
+        if dtype:
+            data_to_plot = data_to_plot.astype(dtype)
+
+        fig = px.imshow(
+            data_to_plot,
+            zmin=0, zmax=500, color_continuous_scale='Blues', text_auto=True,
+            title='Asset Count by Groups',
+            labels=dict(x=xaxis, y=yaxis, color='Asset Count'),
+            width=1100, height=500
+        )
+        return fig
 
 
 if __name__ == "__main__":
--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def get_method_names(obj):
@@ -16,3 +17,53 @@ def convert_to_float(x, multiplier=1.0, default=np.nan):
         except Exception as e:
             print(f'Failed to convert {x} to float: {e}')
             return default
+
+
+def check_group_by_input(func):
+    def wrapper(data, groupby, vars, weight, reset_index=True):
+        import warnings
+        warnings.filterwarnings("ignore")
+        if not isinstance(data, pd.DataFrame):
+            raise ValueError("data must be a pandas DataFrame")
+        if not isinstance(groupby, list):
+            raise ValueError("groupby must be a list")
+        if not isinstance(vars, list):
+            raise ValueError("vars must be a list")
+        if not isinstance(reset_index, bool):
+            raise ValueError("reset_index must be a bool")
+        if len(groupby) == 0:
+            raise ValueError("groupby must have at least one element")
+        if len(vars) == 0:
+            raise ValueError("vars must have at least one element")
+        if not set(groupby).issubset(set(data.columns)):
+            raise ValueError("groupby must be a subset of data's columns")
+        if not set(vars).issubset(set(data.columns)):
+            raise ValueError("vars must be a subset of data's columns")
+        if weight is not None:
+            if not set([weight]).issubset(set(data.columns)):
+                raise ValueError("weight must be a subset of data's columns")
+        if len(set(groupby).intersection(set(vars))) > 0:
+            raise ValueError("groupby and vars must not have any common elements")
+        return func(data, groupby, vars, weight, reset_index)
+
+    return wrapper
+
+
+def assign_market_cap_group(x):
+    """Assign a market cap group based on market cap value in millions of dollars"""
+    if np.isnan(x) or (x is None) or (not isinstance(x, float)):
+        return 'N/A'
+    if x >= 200000:
+        return "Mega"
+    elif 10000 <= x < 200000:
+        return "Large"
+    elif 2000 <= x < 10000:
+        return "Mid"
+    elif 300 <= x < 2000:
+        return "Small"
+    elif 50 <= x < 300:
+        return "Micro"
+    elif 0 < x < 50:
+        return "Nano"
+    else:
+        return "N/A"
